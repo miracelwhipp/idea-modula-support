fc75096e7725515e3739f659dcf11839bdbec0af
miracelwhipp <miracelwhipp@gmx.de>
2014-10-26 19:05:47 +0100

    renamed to complete name

--- ims-plugin/src/main/java/org/modula/helpers/annotators/UnknownQualifiedIdentifierAnnotator.java
+++ ims-plugin/src/main/java/org/modula/helpers/annotators/UnknownQualifiedIdentifierAnnotator.java
@@ -10,20 +10,22 @@ import org.jetbrains.annotations.NotNull;
 import org.modula.helpers.fixes.ImportUnknownDesignatorFix;
 import org.modula.helpers.index.keys.ImplicitEntities;
 import org.modula.helpers.index.keys.ModuleIndex;
+import org.modula.helpers.index.keys.SymbolByModule;
 import org.modula.helpers.index.keys.SymbolIndex;
 import org.modula.helpers.index.stubs.SymbolStubPsiElement;
 import org.modula.parsing.definition.psi.*;
 import org.modula.parsing.psi.ImportMap;
 import org.modula.parsing.psi.ModuleImportStore;
 import org.modula.parsing.psi.SymbolImportStore;
+import org.modula.parsing.utility.ModulaPsiTraversingUtility;
 
 import java.util.Collection;
 import java.util.List;
 
 /**
- * Annotates unknown designators.
+ * Annotates unknown qualified identifiers.
  */
-public class UnknownQualidentAnnotator implements Annotator {
+public class UnknownQualifiedIdentifierAnnotator implements Annotator {
 	@Override
 	public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
 
@@ -44,6 +46,26 @@ public class UnknownQualidentAnnotator implements Annotator {
 
 		String symbolName = definitionSymbolName.getText();
 
+
+		DefinitionDefinitionModule defRootElement = ModulaPsiTraversingUtility.getDefRootElement(element);
+
+		if (defRootElement == null) {
+			return;
+		}
+
+		DefinitionIdent ident = defRootElement.getIdent();
+
+		final GlobalSearchScope global = GlobalSearchScope.allScope(element.getProject());
+
+		Collection<SymbolStubPsiElement> definesOfCurrentModule = SymbolByModule.INSTANCE.get(ident.getText(), element.getProject(), global);
+
+		for (SymbolStubPsiElement symbol : definesOfCurrentModule) {
+			if (symbol.getText().equals(symbolName)) {
+				return;
+			}
+		}
+
+
 		if (imported(symbolName, SymbolImportStore.getSymbolImportMap(element)) || imported(symbolName, ModuleImportStore.getModuleImportMap(element))) {
 			return;
 		}
@@ -54,8 +76,6 @@ public class UnknownQualidentAnnotator implements Annotator {
 
 		Annotation errorAnnotation = holder.createErrorAnnotation(element, "unknown symbol : " + symbolName);
 		errorAnnotation.setHighlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL);
-
-		final GlobalSearchScope global = GlobalSearchScope.allScope(element.getProject());
 
 		Collection<DefinitionDefinitionModule> definitionModuleDefinitions = ModuleIndex.INSTANCE.get(symbolName, element.getProject(), global);
 
